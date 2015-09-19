447f6802a04cb6f57853e85007ad1efa482d1dfb
frostbyte100 <fulbel.leelabari@gmail.com>
2015-09-18 22:39:54 -0500

    started working on get infor from end user input

--- hackisuproject/main.py
+++ hackisuproject/main.py
@@ -18,22 +18,44 @@ import webapp2
 import jinja2
 import os
 from google.appengine.ext import ndb
-
 from google.appengine.api import urlfetch
 import logging
 import json
-
 from operator import eq
 from collections import OrderedDict
 
-
 jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('templates/index.html')
-        self.response.write(template.render())
+        self.response.write(template.render({
+            'search': search,
+            'search1': search1
+        }))
+
+
+    def post(self):
+
+        data_source = urlfetch('http://api.yelp.com/v2/search?term=food&location=San+Francisco')
+
+        search_term = self.request.get("search")
+        search_term = search_term.replace(" ", "+")
+
+
+        search_term1 = self.request.get("search")
+        search_term1 = search_term.replace(" ", "+")
+
+        user_input = urlfetch('http://api.yelp.com/v2/search?term=
+                                                search_term
+                                                &location
+                                                )
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
